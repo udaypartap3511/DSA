26d7da1a021be31a49695273dc2a1fc547e9d1e7
udaypartap3511 <upartap12@gmail.com>
2026-07-08 15:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (64.02%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,12 +11,16 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow=head;
+        
+
         ListNode* fast=head;
+        ListNode* slow=head;
+
         while(fast!=nullptr && fast->next!=nullptr){
             fast=fast->next->next;
             slow=slow->next;
         }
+
         return slow;
     }
 };
