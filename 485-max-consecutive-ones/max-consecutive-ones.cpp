ef2386d18c17083a21d6ca44b41e3a457af90aa5
Uday Partap Singh <upartap12@gmail.com>
2026-07-19 01:10:07 +0530

    Time: 3 ms (18.49%) | Memory: 50.2 MB (68.22%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,20 +6,15 @@ public:
         int maxcnt=0;
 
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                maxcnt=max(cnt,maxcnt);
-                cnt=0;
-            }
-            else if(i==nums.size()-1){
+            if(nums[i]==1){
                 cnt++;
-                maxcnt=max(cnt,maxcnt);
             }
             else{
-                cnt++;
+                cnt=0;
             }
-        }
 
+            maxcnt=max(cnt,maxcnt);
+        }
         return maxcnt;
-        
     }
 };
