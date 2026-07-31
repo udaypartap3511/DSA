d41ec82f617726b3183f9279513877f6170ce937
Uday Partap Singh <upartap12@gmail.com>
2026-07-31 15:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (60.22%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,57 +1,60 @@
 class Solution {
-public: 
-    bool isSafe(vector<string> &chess,int row,int col,int n){
+public:
+
+    bool isSafe(vector<string> &board,int row,int col,int n){
+
         int duprow=row;
         int dupcol=col;
+
         while(row>=0 && col>=0){
-            if(chess[row][col]=='Q') return false;
-            row--;
-            col--;
+            if(board[row--][col--]=='Q') return false;
         }
 
         row=duprow;
         col=dupcol;
 
         while(row>=0 && col<n){
-            if(chess[row][col]=='Q') return false;
-            row--;
-            col++;
+            if(board[row--][col++]=='Q') return false;
         }
 
         row=duprow;
         col=dupcol;
 
         while(row>=0){
-            if(chess[row][col]=='Q') return false;
-            row--;
+            if(board[row--][col]=='Q') return false;
         }
 
         return true;
+
     }
-    void solve(vector<vector<string>> &ans,vector<string> &chess,int row,int col,int n){
+
+    void solve(vector<vector<string>> &ans,vector<string> &board,int row,int n){
+
         if(row==n){
-            ans.push_back(chess);
+            ans.push_back(board);
             return;
         }
 
         for(int col=0;col<n;col++){
-            if(isSafe(chess,row,col,n)){
-                chess[row][col]='Q';
-                solve(ans,chess,row+1,col,n);
-                chess[row][col]='.';
+            if(isSafe(board,row,col,n)){
+                board[row][col]='Q';
+                solve(ans,board,row+1,n);
+                board[row][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> chess;
-        string s(n,'.');
-        for(int i=0;i<n;i++)
-        {
-            chess.push_back(s);
-        }
-        solve(ans,chess,0,0,n);
 
+        vector<vector<string>> ans;
+        vector<string> board;
+
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board.push_back(s);
+        }
+
+        solve(ans,board,0,n);
+        
         return ans;
     }
 };
