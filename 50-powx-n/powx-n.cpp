3079848b08bb804c8f23eb25a0fdfbfa4c5ac66f
Uday Partap Singh <upartap12@gmail.com>
2026-08-08 22:54:04 +0530

    Time: 3 ms (2.19%) | Memory: 8.6 MB (71.56%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        
+        if(n==0 || x==1){
+            return 1;
+        }
+
+        
 
         long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
 
-     if(x==0){
-        return 0;
-      } 
-      else if(N==0){
-        return 1;
-      }
-      else if(N<0){
-        x=1/x;
-        N=-N;
-      }
+        double halfpow=myPow(x,N/2);
 
-      double half=myPow(x,N/2);
-      
-      if(N%2==0){
-        return half*half;
-      }
-      else{
-        return half*half*x;
-      }
+        if(N%2==0){
+            return halfpow*halfpow;
+        }
+        else{
+            return halfpow*halfpow*x;
+        }
     }
 };
