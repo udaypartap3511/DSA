955c3190d79ddd4cc6c07cb81a386f8cffdcd426
Uday Partap Singh <upartap12@gmail.com>
2026-08-13 16:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (69.70%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -22,7 +22,8 @@ public:
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            else if(nums[mid]==nums[mid^1]){
+            else if(mid%2==0 && nums[mid]==nums[mid+1] ||
+                    mid%2==1 && nums[mid]==nums[mid-1]){
                 low=mid+1;
             }
             else{
