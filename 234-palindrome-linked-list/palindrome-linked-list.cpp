c75bfd68ccfbc00947e85b7eb9b767c6d53ae99e
Uday Partap Singh <upartap12@gmail.com>
2026-08-19 13:52:20 +0530

    Time: 4 ms (48.55%) | Memory: 118.1 MB (64.68%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,46 +11,40 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(head==nullptr || head->next==nullptr){
-            return true;
-        }
-        ListNode* dummy=new ListNode(-1,head);
-        ListNode* slow=dummy;
+        
+       if(head==nullptr || head->next==nullptr){
+        return true;
+       }
+        ListNode* dummy= new ListNode(-1,head);
+
         ListNode* fast=dummy;
+        ListNode* slow=dummy;
 
         while(fast!=nullptr && fast->next!=nullptr){
-            fast=fast->next->next;
-            slow=slow->next;
-        }
-        
-        ListNode* Next=slow->next;
-        slow->next=nullptr;
-        ListNode* prev=nullptr;
-        while(Next!=nullptr){
-            ListNode* ne=Next->next;
-            Next->next=prev;
-            prev=Next;
-            Next=ne;
+          fast=fast->next->next;
+          slow=slow->next;
         }
 
-        slow->next=prev;
-        slow=slow->next;
+        ListNode* slownext=slow->next;
+        ListNode* prev=nullptr;
+
+        while(slownext!=nullptr){
+            ListNode* nextnode=slownext->next;
+            slownext->next=prev;
+            prev=slownext;
+            slownext=nextnode;
+        }
 
         fast=head;
-
-        while(slow!=nullptr){
-             if(slow->val!=fast->val){
+        while(prev!=nullptr){
+            if(prev->val!=fast->val){
                 return false;
             }
+
+            prev=prev->next;
             fast=fast->next;
-            slow=slow->next;
-            
-           
-            
         }
 
         return true;
-
-        
     }
 };
