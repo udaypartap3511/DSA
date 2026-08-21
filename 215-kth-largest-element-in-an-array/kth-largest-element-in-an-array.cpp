eda6f61e80fd59c1faab7b6d741c2e678e75ef96
Uday Partap Singh <upartap12@gmail.com>
2026-08-21 15:33:30 +0530

    Time: 46 ms (26.38%) | Memory: 72.5 MB (44.23%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,14 +4,13 @@ public:
 
      priority_queue<int,vector<int>,greater<int>> pq;
      
-     for(int i=0;i<k;i++){
+     for(int i=0;i<nums.size();i++){
         pq.push(nums[i]);
+        if(pq.size()>k){
+            pq.pop();
+        }
      }
 
-     for(int i=k;i<nums.size();i++){
-        pq.push(nums[i]);
-        pq.pop();
-     }
 
      return pq.top();
     
