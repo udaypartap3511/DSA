1b2f7d1eeb8e9cf57b3d9dbf313545d9e492321e
Uday Partap Singh <upartap12@gmail.com>
2026-08-22 14:57:00 +0530

    Time: 5 ms (64.59%) | Memory: 15 MB (86.93%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -21,29 +21,33 @@ public:
         Node* temp=head;
 
         while(temp!=nullptr){
-           Node* Next=temp->next;
-           Node* copy=new Node(temp->val);
-           temp->next=copy;
-           copy->next=Next;
-           temp=temp->next->next;
+            Node* Next=temp->next;
+            temp->next=new Node(temp->val);
+            temp->next->next=Next;
+            temp=temp->next->next;
         }
 
         temp=head;
+
         while(temp!=nullptr){
-           if(temp->random==nullptr) temp->next->random=nullptr;
-           else temp->next->random=temp->random->next;
-           temp=temp->next->next;
+            if(temp->random==nullptr) temp->next->random=nullptr;
+
+            else{
+                temp->next->random=temp->random->next;
+            }
+
+            temp=temp->next->next;
         }
 
+        temp=head;
         Node* dummy=new Node(-1);
         Node* dd=dummy;
 
-        temp=head;
         while(temp!=nullptr){
-            dummy->next=temp->next;
-            temp->next=temp->next->next;
-            temp=temp->next;
-            dummy=dummy->next;
+           dummy->next=temp->next;
+           temp->next=temp->next->next;
+           temp=temp->next;
+           dummy=dummy->next;
         }
 
         return dd->next;
