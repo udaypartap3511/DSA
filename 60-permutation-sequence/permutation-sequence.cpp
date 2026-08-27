eed8f68cc5d1653f2bff7072a623a663dcc27b3a
Uday Partap Singh <upartap12@gmail.com>
2026-08-27 14:10:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (17.97%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-  
     string getPermutation(int n, int k) {
-
-        vector<int> v;
+        
         int fact=1;
+        vector<int> v;
         for(int i=1;i<n;i++){
             fact*=i;
             v.push_back(i);
         }
         v.push_back(n);
-        string s;
-        k=k-1;
+        k--;
+        string s="";
+
         while(true){
             s+=to_string(v[k/fact]);
             v.erase(v.begin()+k/fact);
@@ -20,12 +20,9 @@ public:
             }
             k=k%fact;
             fact=fact/v.size();
-            
-            
         }
 
         return s;
 
-        
     }
 };
