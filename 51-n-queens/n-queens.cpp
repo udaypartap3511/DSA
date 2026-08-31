6dee32d76b73a1d8af9afd68528ba9bf43f79d04
Uday Partap Singh <upartap12@gmail.com>
2026-08-31 14:25:32 +0530

    Time: 2 ms (73.13%) | Memory: 10.1 MB (93.97%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    bool isSafe(vector<string>&board,int row,int col,int n){
+    bool isSafe(vector<string> &board,int row,int col,int n){
 
         int duprow=row;
         int dupcol=col;
+
         while(row>=0 && col>=0){
             if(board[row--][col--]=='Q') return false;
         }
 
         row=duprow;
         col=dupcol;
+
         while(row>=0 && col<n){
             if(board[row--][col++]=='Q') return false;
         }
+
         row=duprow;
         col=dupcol;
+
         while(row>=0){
             if(board[row--][col]=='Q') return false;
         }
 
         return true;
-
     }
-    void solve(vector<vector<string>>&ans,vector<string> &board,int row,int n){
+    void solve(vector<vector<string>> & ans,vector<string> &board,int row,int n){
 
         if(row==n){
             ans.push_back(board);
@@ -43,16 +46,15 @@ public:
         string s;
         for(int i=0;i<n;i++){
             s+='.';
-        }
+        } 
+
         for(int i=0;i<n;i++){
             board.push_back(s);
         }
-
-        vector<vector<string>> ans;
-
+         
+         vector<vector<string>> ans;
         solve(ans,board,0,n);
-
-        return ans;
         
+        return ans;
     }
 };
