5a5f9ebd29ecb14a852b23cc6594c8b65ee7b9d4
Uday Partap Singh <upartap12@gmail.com>
2026-09-08 14:24:04 +0530

    Time: 27 ms (8.31%) | Memory: 23 MB (5.41%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-     int t[301];
-    bool solve(int ind,string s,unordered_set<string> &mp){
-
+    int t[301];
+    bool solve(int ind,unordered_set<string> & st,string s){
         int n=s.length();
         if(ind==n){
-            return t[ind]= true;
+            return t[ind]=true;
         }
         if(t[ind]!=-1){
             return t[ind];
         }
-        // if(mp.find(s)!=mp.end()){
-        //     return t[ind]=true;
-        // }
-
-        for(int l=1;l<=n;l++){
-            string temp=s.substr(ind,l);
-            if(mp.find(temp)!=mp.end() && solve(ind+l,s,mp)){
-               return t[ind]=true;
+        
+        for(int len=1;len<=n;len++){
+            string temp=s.substr(ind,len);
+            if(st.find(temp)!=st.end() && solve(ind+len,st,s)){
+                return t[ind]=true;
             }
         }
 
         return t[ind]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-
-        unordered_set<string> mp;
+        
         memset(t,-1,sizeof(t));
+        unordered_set<string> st;
 
         for(auto i:wordDict){
-            mp.insert(i);
+            st.insert(i);
         }
 
-        if(solve(0,s,mp)==true) return true;
+        if(solve(0,st,s))return true;
 
         return false;
-        
     }
 };
