868d3ab2a261fbceba4c1efe131e7e34cd0c97a2
Uday Partap Singh <upartap12@gmail.com>
2026-09-10 22:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (38.31%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        
+      
         int n=nums.size();
         int low=1;
-        int high=nums.size()-2;
-
-        if(nums.size()>1 && nums[0]!=nums[1]){
-            return nums[0];
-        }
-         if(nums.size()>1 && nums[n-1]!=nums[n-2]){
-            return nums[n-1];
-        }
+        int high=n-1;
 
         if(nums.size()==1){
             return nums[0];
         }
 
-        int ele;
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
 
         while(low<=high){
-
             int mid=(low+high)/2;
 
-            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
+
             else if(nums[mid]==nums[mid^1]){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
+            
         }
-
-        return -1;
+          
+          return -1;
     }
 };
